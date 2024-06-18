3cc0db5552b4cfa90a79943ada26e08087f37f93
Isaac Corbrey <isaac.corbrey@doitbest.com>
2024-06-18 11:32:13 -0400

    feat: Add basic C# support

---
+++ demos/cs.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Kek {
+    public static class Program {
+        public static void Main() {
+            System.Console.WriteLine("kek");
+
+            if (true) {
+                return null;
+            }
+        }
+    }
+
+    public interface IFoo {}
+}
